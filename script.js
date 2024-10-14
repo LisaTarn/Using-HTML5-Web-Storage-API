0de1d7d06c4ec912e0b68a0bdf78afe2e6c98edc
LisaTarn <150932484+LisaTarn@users.noreply.github.com>
2024-10-14 10:24:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const saveButton = document.getElementById("save-btn");
+const clearButton = document.getElementById("clear-btn");
+
+saveButton.addEventListener ("click", displayUserName);
+
+function displayUserName (){
+  const displayName = document.getElementById("username").value
+  localStorage.setItem("display-name", displayName);
+
+  const savedDisplayName = localStorage.getItem("display-name");
+  document.getElementById("display-name").innerHTML = savedDisplayName;
+}
+
+clearButton.addEventListener ("click", clearUserName);
+
+function clearUserName(){
+localStorage.clear();
+document.getElementById("display-name").innerHTML = "   ";
+}
+
