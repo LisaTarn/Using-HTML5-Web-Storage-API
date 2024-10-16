eda5fd224b3259054f81aab9d11ab627b11f4b2b
LisaTarn <150932484+LisaTarn@users.noreply.github.com>
2024-10-15 22:01:43 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,23 @@ const saveButton = document.getElementById("save-btn"); //setting up save button
 const clearButton = document.getElementById("clear-btn"); //setting up clear button
 
 saveButton.addEventListener ("click", displayUserName); //adding event listener so input will save to local storage on click
+window.addEventListener("load", showCookieValue); //adding event listener to load cookie 
 
 function displayUserName (){  //function to save to local storage
   const displayName = document.getElementById("username").value
   localStorage.setItem("display-name", displayName);
-
   const savedDisplayName = localStorage.getItem("display-name");
   document.getElementById("display-name").innerHTML = savedDisplayName;
-}
+  document.cookie="UserName:" + savedDisplayName; //sets a cookie
+  }
+
+  function showCookieValue() { //function to show the cookie
+    if (localStorage.getItem("display-name")){
+    const output = document.cookie;
+    document.getElementById("display-name").innerHTML= output;}
+  else{
+    document.getElementById("display-name").innerHTML = "";
+  }}
 
 clearButton.addEventListener ("click", clearUserName); //adding event listener to clear button to clear local storage
 
@@ -17,6 +26,4 @@ function clearUserName(){  //function to clear local storage
 localStorage.clear();
 document.getElementById("display-name").innerHTML = "   ";
 }
-
-
 
